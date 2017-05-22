948a6ecbba2147b47b75023d0c332fb5683a2caf
Fangning Shao <fangning.shao@gmail.com>
2017-05-22 02:11:14 -0700

    Multiprocessing crawl all poems

--- scrape/main.py
+++ scrape/main.py
@@ -4,6 +4,7 @@ import os
 import re
 from bs4 import BeautifulSoup
 import urllib2
+import multiprocessing as mp
 
 
 def fetch(url):
@@ -45,7 +46,7 @@ def get_dynasties():
     for a in dynasties:
         url = ROOT_PAGE + a.get('href')
         name = a.text
-        print 'Get dynasty:', name.encode('utf8')
+        # print 'Get dynasty:', name.encode('utf8')
         dynasty_name_urls.append((name, url))
 
     return dynasty_name_urls
@@ -59,10 +60,9 @@ def save_dynasties(dynasties):
     f.close()
 
 
-
 def get_authors(dynasty):
     dynasty_name, url = dynasty
-    print dynasty_name, url
+    print 'Getting authors for dynasty', dynasty_name, url
 
     dynasty_soup = soup(url)
 
@@ -74,7 +74,7 @@ def get_authors(dynasty):
         name = a.text
         url = ROOT_PAGE + a.get('href')
         author_name_urls.append((name, url))
-        print name, url
+        # print name, url
 
     return author_name_urls
 
@@ -90,52 +90,64 @@ def save_authors(dynasty, authors):
 
     f.close()
 
+
 def get_poems(author):
-    dynasty_name, url = dynasty
+    author_name, url = author
+    print 'Getting poems for author', author_name, url
+    author_soup = soup(url)
+    poem_soup = author_soup.find('div', 'poem')
+    divs = poem_soup.find_all('div')
+    all_poems = []
+    for d in divs:
+        if d.get('id', '').startswith('item_'):  # is a poem
+            p_title = find_text(d, 'title')
+            # strip out bad characters after name
+            p_title = re.sub('显示自动注释'.decode('utf8'), '', p_title).strip()
+
+            # don't include tabs and newlines
+            p_content = find_text(d, 'content').replace(
+                '\t', ' ').replace('\n', '')
+
+            all_poems.append((p_title, p_content))
+
+            print 'POEM TITLE:', p_title
+            # print 'CONTENT:', p_content
+
+    return all_poems
 
 
-def save_poems(poems):
-    pass
+def save_poems(dynasty, author, poems):
+    folder = DATA_FOLDER + '%s/poems/' % dynasty
+    mkdir(folder)
+    print 'Saving poems for author', author
+    f = open(folder + '%s.tsv' % author, 'w')
+    for d in poems:
+        f.write('\t'.join(d).encode('utf8') + '\n')
 
-# p1 = soup(dynasty_name_urls[0][1])
-
-
-# d = p1.find('div', 'list1')
-# authors = d.find_all('a')
-# author_name_urls = []
-# for a in authors:
-#     author_name_urls.append((a.text, ROOT_PAGE + a.get('href')))
-
-# author_name_urls
+    f.close()
 
 
-# test_author_name, url = author_name_urls[1]
-# print test_author_name, url
-# test_author_soup = soup(url)
+def process_author_poems(dynasty, authors, i):
+    print('%s: Process %d has %d authors' % (dynasty, i, len(authors)))
+    for author in authors:
+        poems = get_poems(author)
+        save_poems(dynasty, author[0], poems)
 
 
-# def get_all_poems_for_author(author_soup):
-#     poem_soup = author_soup.find('div', 'poem')
-#     divs = poem_soup.find_all('div')
-#     for d in divs:
-#         if d.get('id', '').startswith('item_'):  # is a poem
-#             p_title = find_text(d, 'title')
-#             p_title = re.sub('显示自动注释', '', p_title).strip()
-# #             print 'POEM TITLE:', p_title
-#             p_content = find_text(d, 'content')
-# #             print 'CONTENT:', p_content
-#             yield p_title, p_content
+def mp_process_all_authors(dynasty, authors):
+    processes = []
+    num_processes = 8
+    chunk_size = len(authors) / num_processes + 1
+    for i in range(num_processes):
+        authors_chunk = authors[i * chunk_size: (i + 1) * chunk_size]
+        p = mp.Process(target=process_author_poems,
+                       args=(dynasty, authors_chunk, i))
+        p.start()
+        processes.append(p)
 
-
-# print test_author_name
-# for t, c in get_all_poems_for_author(test_author_soup):
-#     pass
-
-
-# for name, url in author_name_urls:
-#     s = soup(url)
-#     for t, c in get_all_poems_for_author(s):
-#         print (name, t)
+    # wait until all finished
+    for p in processes:
+        p.join()
 
 
 # ####################
@@ -150,6 +162,8 @@ if __name__ == '__main__':
         authors = get_authors(dynasty)
         save_authors(dynasty[0], authors)
 
-        for a in authors:
-            poems = get_poems(a)
-            save_poems(poems)
+        # for a in authors:
+        #     poems = get_poems(a)
+        #     save_poems(dynasty[0], a[0], poems)
+
+        mp_process_all_authors(dynasty[0], authors)
